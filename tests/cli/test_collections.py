775fa28454bb8df8ba6fc55a5fa7cb7ffe93d5be
Bazlur Rahman <anmbazlur@dnastack.com>
2025-03-04 13:23:29 -0500

    [CU-86b40c2tc] Add `--endpoint-id` to ensure tof fix the tests

--- tests/cli/test_collections.py
+++ tests/cli/test_collections.py
@@ -18,7 +18,7 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
         """ This is to test with the current implementation of collection service. """
 
         # Test listing collection
-        collections = self.simple_invoke('collections', 'list')
+        collections = self.simple_invoke('collections', 'list', '--endpoint-id=collection-service')
         self.assertGreaterEqual(len(collections), 1, 'Must have at least one collection for this test')
 
         last_error = None
@@ -36,7 +36,7 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
                 tables = self.simple_invoke('cs',
                                             'tables',
                                             'list',
-                                            '-c', collection_slug_name)
+                                            '-c', collection_slug_name, '--endpoint-id=collection-service')
                 self.assertGreaterEqual(len(tables), 0)
 
                 max_size = 10
@@ -45,13 +45,13 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
                 items_from_direct_query = self.simple_invoke('collections',
                                                              'list-items',
                                                              '--limit', str(max_size),
-                                                             '--collection', collection_slug_name)
+                                                             '--collection', collection_slug_name, '--endpoint-id=collection-service')
                 self.assertLessEqual(len(items_from_direct_query), max_size, f'Expected upto {max_size} rows')
 
                 # CSV version
                 for table in tables:
                     query = f"SELECT * FROM ({table['name']}) LIMIT {max_size}"
-                    result = self.invoke('cs', 'query', '-c', collection_slug_name, '-o', 'csv', query)
+                    result = self.invoke('cs', 'query', '-c', collection_slug_name, '-o', 'csv', query, '--endpoint-id=collection-service')
                     lines = result.output.split('\n')
                     self.assertLessEqual(len(lines), max_size + 1, f'Expected upto {max_size} lines, excluding headers')
                     for line in lines:
@@ -63,7 +63,7 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
                 items_from_command = self.simple_invoke('collections',
                                                         'list-items',
                                                         '-c', collection_slug_name,
-                                                        '-l', str(max_size))
+                                                        '-l', str(max_size), '--endpoint-id=collection-service')
                 self.assertLessEqual(len(items_from_command), max_size, f'Expected upto {max_size} rows')
 
                 common_ids = set([i['id'] for i in items_from_direct_query])\
@@ -89,7 +89,7 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
         there should not be an additional auth prompt if the target per-collection data-connect endpoint is registered.
         """
         self.invoke('use', self._explorer_hostname)
-        collections = self.simple_invoke('collections', 'list')
+        collections = self.simple_invoke('collections', 'list', '--endpoint-id=collection-service')
         self.assert_not_empty(collections, 'No collection available')
         last_error = None
         for c in collections:
@@ -97,7 +97,7 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
                 self.simple_invoke('collections',
                                    'query',
                                    '-c', c['slugName'],
-                                   'SELECT 1')
+                                   'SELECT 1', '--endpoint-id=collection-service')
 
                 return
             except DataConnectError as e:
@@ -124,4 +124,5 @@ class TestCollectionsCommand(DeprecatedPublisherCliTestCase):
             self.invoke('collections',
                         'query',
                         '-c', f'foobar-{uuid4()}',
-                        'SELECT 1')
+                        'SELECT 1',
+                        '--endpoint-id=collection-service')
