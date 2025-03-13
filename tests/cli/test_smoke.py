008bdfc6b397679dc2dcdc6cfbde9c1b08552d0e
Bazlur Rahman <anmbazlur@dnastack.com>
2025-03-13 17:24:35 -0400

    [CU-86b40c2tc] Add `--endpoint-id` parameter to `collections` commands in tests

--- tests/cli/test_smoke.py
+++ tests/cli/test_smoke.py
@@ -13,12 +13,12 @@ class TestSmoke(DeprecatedPublisherCliTestCase):
     def test_happy_path(self):
         self.invoke('use', self.explorer_urls[0])
 
-        for collection in self.simple_invoke('collections', 'list'):
+        for collection in self.simple_invoke('collections', 'list', '--endpoint-id=collection-service'):
             collection_id = collection['slugName']
             try:
-                self.invoke('collections', 'tables', 'list', '--collection', collection_id)
-                self.invoke('collections', 'query', '-c', collection_id, 'SELECT 1')
-                self.invoke('collections', 'query', '--collection', collection_id, 'SELECT 1')
+                self.invoke('collections', 'tables', 'list', '--collection', collection_id, '--endpoint-id=collection-service')
+                self.invoke('collections', 'query', '-c', collection_id, 'SELECT 1', '--endpoint-id=collection-service')
+                self.invoke('collections', 'query', '--collection', collection_id, 'SELECT 1','--endpoint-id=collection-service')
                 return
             except:
                 pass
