8f113dbedeb62951f65e8cc0087625c7f375f352
Angelo Genovese <angelo@dnastack.com>
2026-04-17 12:38:20 -0400

    [CU-86b90ukc7] Add DNASTACK_METRICS_ENABLED feature flag

--- tests/unit/test_feature_flags.py
+++ tests/unit/test_feature_flags.py
@@ -1,9 +1,17 @@
 import importlib
 import os
 
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def clean_metrics_env():
+    os.environ.pop('DNASTACK_METRICS_ENABLED', None)
+    yield
+    os.environ.pop('DNASTACK_METRICS_ENABLED', None)
+
 
 def test_metrics_enabled_defaults_to_false():
-    os.environ.pop('DNASTACK_METRICS_ENABLED', None)
     import dnastack.feature_flags as ff
     importlib.reload(ff)
     assert ff.metrics_enabled is False
@@ -14,4 +22,3 @@ def test_metrics_enabled_is_true_when_set_to_1():
     import dnastack.feature_flags as ff
     importlib.reload(ff)
     assert ff.metrics_enabled is True
-    os.environ.pop('DNASTACK_METRICS_ENABLED', None)
